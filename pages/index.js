7e6c5292393191221df8ef7b43eb81033aa92e17
ivandjoh <ivandi.djoh@gmail.com>
2023-04-13 12:48:38 +0800

    add multiple json array

--- pages/index.js
+++ pages/index.js
@@ -9,23 +9,29 @@ export async function getStaticProps() {
 
     return {
         props: {
-            text: jsonContent.text,
+            dataArray: jsonContent,
         },
     };
 }
 
-function Home({ text }) {
-    const [updatedString, setUpdatedString] = useState('');
+function Home({ dataArray }) {
+    const [updatedStrings, setUpdatedStrings] = useState([]);
 
     useEffect(() => {
-        const updated = text.replace(/(<ads><\/ads>)/, '<ads>haha</ads>');
-        setUpdatedString(updated);
-    }, [text]);
+        const updated = dataArray.map((item) =>
+            item.text.replace(/(<ads><\/ads>)/, '<ads>haha</ads>')
+        );
+        setUpdatedStrings(updated);
+    }, [dataArray]);
 
     return (
         <div>
-            <h1>Updated String:</h1>
-            <p>{updatedString}</p>
+            <h1>Updated Strings:</h1>
+            <ul>
+                {updatedStrings.map((string, index) => (
+                    <li key={index}>{string}</li>
+                ))}
+            </ul>
         </div>
     );
 }
